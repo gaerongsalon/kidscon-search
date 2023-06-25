bb591ce20f983f31dc9836cc3f131c41e0d32244
jaeyoung.choi <lactrious@gmail.com>
2023-06-25 09:19:51 +0900

    Apply feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import {
+  Autocomplete,
   Badge,
+  Box,
   Button,
   Card,
   Group,
   Image,
+  List,
+  MediaQuery,
   SimpleGrid,
   Space,
   Text,
-  TextInput,
 } from "@mantine/core";
 import { useEffect, useMemo, useState } from "react";
 
@@ -17,62 +20,99 @@ import { useDebouncedValue } from "@mantine/hooks";
 
 type Row = (typeof data)[0];
 
+function unique(arr: string[]) {
+  return [...new Set(arr)];
+}
+function flatUnique(arr: string[]) {
+  return unique(
+    arr
+      .flatMap((index) => index.split(/[ ,]/g))
+      .map((e) => e.trim())
+      .filter(Boolean)
+  );
+}
+
 const indexed = data.map((row) => ({
   ...row,
-  index: [...(row.keywords ?? []), ...(row.characters ?? []), row.title],
+  index: unique([
+    ...(row.keywords ?? []),
+    ...(row.characters ?? []),
+    row.title,
+  ]),
 }));
+const allKeywords = flatUnique(
+  data.flatMap((row) => [...(row.keywords ?? []), ...(row.characters ?? [])])
+);
 
 function App() {
   const [keyword, setKeyword] = useState("");
-  const result = useMemo(() => {
-    const rs = keyword
+  const rs = useMemo(() => {
+    return keyword
       .split(/[ ,]/g)
       .map((e) => e.trim())
       .filter(Boolean)
       .map((e) => createFuzzyMatcher(e));
+  }, [keyword]);
+  const result = useMemo(() => {
     return indexed.filter((row) =>
       rs.every((r) => row.index.some((e) => r.test(e)))
     );
-  }, [keyword]);
+  }, [rs]);
+  const suggestion = useMemo(() => {
+    return allKeywords.filter((e) => rs.every((r) => r.test(e)));
+  }, [rs]);
   return (
     <>
       <div>
-        <SearchTextBox setKeyword={setKeyword} />
+        <SearchTextBox setKeyword={setKeyword} suggestion={suggestion} />
       </div>
 
       <Space h="md" />
-      <SimpleGrid
-        cols={4}
-        spacing="xl"
-        breakpoints={[
-          { maxWidth: "62rem", cols: 3, spacing: "md" },
-          { maxWidth: "48rem", cols: 2, spacing: "sm" },
-          { maxWidth: "36rem", cols: 1, spacing: "sm" },
-        ]}
-      >
-        {result.map((row) => (
-          <ResultItem row={row} />
-        ))}
-      </SimpleGrid>
+      <MediaQuery query="(max-width: 36em)" styles={{ display: "none" }}>
+        <SimpleGrid
+          cols={4}
+          breakpoints={[
+            { maxWidth: "62rem", cols: 3, spacing: "md" },
+            { maxWidth: "48rem", cols: 2, spacing: "sm" },
+          ]}
+          spacing="xl"
+        >
+          {result.map((row) => (
+            <ResultItem row={row} />
+          ))}
+        </SimpleGrid>
+      </MediaQuery>
+      <MediaQuery query="(min-width: 36em)" styles={{ display: "none" }}>
+        <List spacing="xl">
+          {result.map((row) => (
+            <ResultListItem row={row} />
+          ))}
+        </List>
+      </MediaQuery>
     </>
   );
 }
 
 function SearchTextBox({
   setKeyword,
+  suggestion,
 }: {
   setKeyword: (keyword: string) => void;
+  suggestion: string[];
 }) {
   const [value, setValue] = useState("");
-  const [keyword] = useDebouncedValue(value, 500);
+  const [keyword] = useDebouncedValue(value, 300);
   useEffect(() => {
     setKeyword(keyword);
   }, [keyword, setKeyword]);
+
   return (
-    <TextInput
+    <Autocomplete
       label="🧒🪄 키워드를 입력하세요"
       value={value}
-      onChange={(event) => setValue(event.currentTarget.value)}
+      size="xl"
+      onChange={setValue}
+      data={suggestion}
     />
   );
 }
@@ -81,16 +121,15 @@ function ResultItem({ row }: { row: Row }) {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <Card.Section>
-        <Image
-          src={`https://i3.ytimg.com/vi/${row.youtubeId}/hqdefault.jpg`}
-          alt="Norway"
-        />
+        <Image src={`https://i3.ytimg.com/vi/${row.youtubeId}/hqdefault.jpg`} />
       </Card.Section>
 
       <Group position="apart" mt="md" mb="xs">
-        <Text weight={500}>{row.title}</Text>
+        <Text weight={500}>
+          {row.serial}. {row.title}
+        </Text>
         <Badge color="pink" variant="light">
-          {row.category} {row.season} {row.serial}
+          {row.category} {row.season}
         </Badge>
       </Group>
 
@@ -110,6 +149,36 @@ function ResultItem({ row }: { row: Row }) {
         유튜브에서 보기
       </Button>
     </Card>
+  );
+}
+
+function ResultListItem({ row }: { row: Row }) {
+  return (
+    <List.Item
+      icon={
+        <Image
+          src={`https://i3.ytimg.com/vi/${row.youtubeId}/hqdefault.jpg`}
+          width={120}
+          alt="Norway"
+        />
+      }
+      onClick={() => window.open(`https://youtu.be/${row.youtubeId}`)}
+    >
+      <Badge mt="xs">
+        {row.category} {row.season}
+      </Badge>
+      <Text weight={500} mb="xs">
+        {row.serial}. {row.title}
+      </Text>
+
+      <Box w={210}>
+        <Text size="sm" color="dimmed" truncate>
+          {flatUnique((row.characters ?? []).concat(row.keywords ?? [])).join(
+            " "
+          )}
+        </Text>
+      </Box>
+    </List.Item>
   );
 }
 
